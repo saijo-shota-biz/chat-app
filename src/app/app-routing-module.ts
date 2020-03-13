2d138dee332b17df40272d3587ce7f88a0aedeed
saijo-shota-biz <saijo.shota.biz@gmail.com>
2020-03-13 23:43:16 +0900

    add pages

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ChatComponent } from './chat/chat.component';
+import { AccountComponent } from './account/account.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'chat', component: ChatComponent },
+  { path: 'account', component: AccountComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { TopNavComponent } from './top-nav/top-nav.component';
 import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
 
 import { AppRoutingModule } from './app-routing-module';
+import { HomeComponent } from './home/home.component';
+import { ChatComponent } from './chat/chat.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavComponent,
-    BottomNavComponent
+    BottomNavComponent,
+    HomeComponent,
+    ChatComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
