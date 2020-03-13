d6b4c976836c1ba70e074bdab97ca49ec7cfbeb1
saijo-shota-biz <saijo.shota.biz@gmail.com>
2020-03-13 23:34:24 +0900

    add layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
+
+import { AppRoutingModule } from './app-routing-module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    BottomNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
